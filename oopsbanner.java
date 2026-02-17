48a70bdf1f094cd2083e198b072f1bcc1a5cc55e
adi9507 <aryaman.shroff@gmail.com>
2026-02-17 13:39:44 +0530

    uc2 done

--- oopsbanner.java
+++ oopsbanner.java
@@ -1,7 +1,26 @@
-class oopsbanner
-{
-public static void main(String args[])
-{
-System.out.println("This is the Main Branch");
-}
+public class oopsbanner {
+    public static void main(String[] args) {
+
+        // Line 1
+        System.out.println("  *****  " + "  *****  " + "  ****** " + "  ****** ");
+
+        // Line 2
+        System.out.println(" *     * " + " *     * " + " *     * " + " *       ");
+
+        // Line 3
+        System.out.println(" *     * " + " *     * " + " *     * " + " *       ");
+
+        // Line 4
+        System.out.println(" *     * " + " *     * " + " ******  " + "  *****  ");
+
+        // Line 5
+        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+
+        // Line 6
+        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+
+        // Line 7
+        System.out.println("  *****  " + "  *****  " + " *       " + "  *****  ");
+
+    }
 }
