51eb3b2fcbe73c2b187fff833958c9239d0a8c03
adi9507 <aryaman.shroff@gmail.com>
2026-03-11 13:15:51 +0530

    UC 4 added

--- oopsbanner.java
+++ oopsbanner.java
@@ -2,53 +2,20 @@ public class oopsbanner {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ",
-                " ******** ",
-                " ******** ",
-                " ******** ",
-                " *******  "
-        ));
+        // Step 1: Define a String array for 7 banner lines
+        String[] lines = new String[7];
 
-        System.out.println(String.join(" ",
-                "*        *",
-                "*        *",
-                "*        *",
-                "*        *"
-        ));
+        // Step 2: Populate the array using String.join()
+        lines[0] = String.join("   ", " *** ", " *** ", " **** ", " **** ");
+        lines[1] = String.join("   ", "*   *", "*   *", "*   *", "*     ");
+        lines[2] = String.join("   ", "*   *", "*   *", "*   *", "*     ");
+        lines[3] = String.join("   ", "*   *", "*   *", " **** ", " ***  ");
+        lines[4] = String.join("   ", "*   *", "*   *", "*     ", "    * ");
+        lines[5] = String.join("   ", "*   *", "*   *", "*     ", "*   * ");
+        lines[6] = String.join("   ", " *** ", " *** ", "*     ", " ***  ");
 
-        System.out.println(String.join(" ",
-                "*        *",
-                "*        *",
-                "*        *",
-                "*         "
-        ));
-
-        System.out.println(String.join(" ",
-                "*        *",
-                "*        *",
-                "********* ",
-                " *******  "
-        ));
-
-        System.out.println(String.join(" ",
-                "*        *",
-                "*        *",
-                "*         ",
-                "        * "
-        ));
-
-        System.out.println(String.join(" ",
-                "*        *",
-                "*        *",
-                "*         ",
-                "*       * "
-        ));
-
-        System.out.println(String.join(" ",
-                " ******** ",
-                " ******** ",
-                "*         ",
-                " *******  "
-        ));
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
